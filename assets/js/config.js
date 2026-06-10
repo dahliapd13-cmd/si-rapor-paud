1b0264855d714a66a9998a2edf73336ba267b8e3
dahliapd13-cmd <dahliapd13@gmail.com>
2026-06-10 11:12:58 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,10 @@
 const CONFIG = {
+
   APP_NAME: "SiRapor PAUD",
+
   SCHOOL_NAME: "PAUD DAHLIA XIII",
-  API_URL: "https://script.google.com/macros/s/AKfycbzele7aowJOmJeg6VidkguSFrW72D8zMhx7XhnmKAg4uvDj80fcdC6AXQFNW3Fz1Cqv/exec"
+
+  API_URL:
+  "https://script.google.com/macros/s/AKfycbzele7aowJOmJeg6VidkguSFrW72D8zMhx7XhnmKAg4uvDj80fcdC6AXQFNW3Fz1Cqv/exec"
+
 };
