31b20b777299ae678865c0500a4b6bf82ce80532
dahliapd13-cmd <dahliapd13@gmail.com>
2026-06-10 13:52:29 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,4 @@
-const CONFIG = {
-
-  API_URL:
-  "https://script.google.com/macros/s/AKfycbzele7aowJOmJeg6VidkguSFrW72D8zMhx7XhnmKAg4uvDj80fcdC6AXQFNW3Fz1Cqv/exec"
-
-};
+const response =
+    await fetch(
+      "https://script.google.com/macros/s/AKfycbzele7aowJOmJeg6VidkguSFrW72D8zMhx7XhnmKAg4uvDj80fcdC6AXQFNW3Fz1Cqv/exec?action=getDashboard"
+    );
