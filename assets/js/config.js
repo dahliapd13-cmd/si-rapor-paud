ef545bf516ee9a8411bffeeeaec8dfc008870f69
dahliapd13-cmd <dahliapd13@gmail.com>
2026-06-10 15:06:01 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,20 @@
-const response =
-    await fetch(
-      "https://script.google.com/macros/s/AKfycbzele7aowJOmJeg6VidkguSFrW72D8zMhx7XhnmKAg4uvDj80fcdC6AXQFNW3Fz1Cqv/exec?action=getDashboard"
-    );
+const API_URL = "https://script.google.com/macros/s/AKfycbzele7aowJOmJeg6VidkguSFrW72D8zMhx7XhnmKAg4uvDj80fcdC6AXQFNW3Fz1Cqv/exec";
+
+async function prosesLogin(user, pass) {
+    try {
+        // Menggunakan metode GET sesuai router doGet Anda
+        const response = await fetch(`${API_URL}?action=login&username=${user}&password=${pass}`);
+        const result = await response.json();
+        
+        if (result.status === true) {
+            console.log("Sukses login:", result.data);
+            // Redirect ke halaman dashboard
+            window.location.href = "dashboard.html";
+        } else {
+            alert("Gagal Login: " + result.message);
+        }
+    } catch (error) {
+        console.error("Server Error:", error);
+        alert("Gagal terhubung ke server.");
+    }
+}
